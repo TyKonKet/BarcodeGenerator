3cc29f94f34218cc251e923be5c7c57cb11cb3a7
TyKonKet <matteoautieri@live.it>
2025-01-18 23:26:27 +0100

    Add barcode generation features and example class
    Introduces a new `Example` class with a method to create and export a Code93 barcode image. Updates barcode generation in `Program.cs` to use EAN-13 encoding and adds a `RenderBarcodes` method for generating barcodes with various font styles. Enhances customization options for barcode appearance by utilizing the `TyKonKet.BarcodeGenerator.Fonts` namespace.

---
+++ Tests/TyKonKet.BarcodeGenerator.Playground/Example.cs
@@ -0,0 +1,39 @@
+﻿using SkiaSharp;
+using TyKonKet.BarcodeGenerator;
+
+internal static class Example
+{
+    private static void CreateBarcodeImage()
+    {
+        // Set up the barcode generator with specific options
+        using var barcodeGenerator = new Barcode(options =>
+        {
+            // Set the type of barcode to Code93
+            options.Type = BarcodeTypes.Code93;
+            // Set the height of the barcode
+            options.Height = 30;
+            // Set the scaling factor for the barcode
+            options.Scaling = 10;
+            // Set the margins around the barcode
+            options.Margins = 2;
+            // Set the background color of the barcode to transparent
+            options.BackgroundColor = SKColors.Transparent;
+            // Set the foreground color of the barcode to black
+            options.ForegroundColor = SKColors.Black;
+            // Enable rendering of text below the barcode
+            options.RenderText = true;
+            // Use the specified font family and style for the text
+            options.UseTypeface("Arial", SKFontStyle.Normal);
+        });
+
+        // Encode the barcode with the specified string
+        barcodeGenerator.Encode("THE-BARCODE");
+
+        // Export the barcode image to a file
+        // The file path can include the following keywords:
+        // - {barcode}: The barcode string.
+        // - {format}: The image format (e.g., png, jpg).
+        // - {quality}: The image quality (1-100).
+        barcodeGenerator.Export("path/{barcode}_{quality}.{format}", SKEncodedImageFormat.Png, 100);
+    }
+}
--- Tests/TyKonKet.BarcodeGenerator.Playground/Program.cs
+++ Tests/TyKonKet.BarcodeGenerator.Playground/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using TyKonKet.BarcodeGenerator.Fonts;
 
 namespace TyKonKet.BarcodeGenerator.Playground
 {
@@ -10,23 +11,32 @@ namespace TyKonKet.BarcodeGenerator.Playground
     {
         private static void Main()
         {
+            //for (int i = 0; i <= 15; i++)
+            //{
+            //    RenderBarcodes((FontFamilies)i);
+            //}
+
+            //return;
+
             var sw = new Stopwatch();
             sw.Start();
 
             var bc = new Barcode(o =>
             {
-                o.Encode = BarcodeEncodings.Upca;
+                o.Type = BarcodeTypes.Ean13;
                 o.Height = 30;
-                o.Scale = 10;
+                o.Scaling = 10;
                 o.Margins = 2;
-                o.BackgroundColor = SKColors.Transparent;
-                o.Color = SKColors.Black;
-                o.Font = "Arial";
-                o.FontStyle = SKFontStyle.Normal;
+                o.BackgroundColor = SKColors.White;
+                o.ForegroundColor = SKColors.Black;
                 o.RenderText = true;
+                o.UseTypeface(FontFamilies.Arial, SKFontStyle.Normal);
+                //o.UseTypeface("Arial", SKFontStyle.Normal);
             });
 
-            bc.Encode("72527273070");
+            const string barcode = "978020137962";
+
+            bc.Encode(barcode);
             Console.WriteLine($"Barcode generated in {sw.ElapsedMilliseconds}ms");
 
             sw.Restart();
@@ -36,7 +46,7 @@ namespace TyKonKet.BarcodeGenerator.Playground
 
             Console.WriteLine();
             sw.Restart();
-            bc.Encode("72527273070");
+            bc.Encode(barcode);
             Console.WriteLine($"Barcode generated in {sw.ElapsedMilliseconds}ms");
 
             sw.Restart();
@@ -46,7 +56,7 @@ namespace TyKonKet.BarcodeGenerator.Playground
 
             Console.WriteLine();
             sw.Restart();
-            bc.Encode("72527273070");
+            bc.Encode(barcode);
             Console.WriteLine($"Barcode generated in {sw.ElapsedMilliseconds}ms");
 
             sw.Restart();
@@ -56,7 +66,7 @@ namespace TyKonKet.BarcodeGenerator.Playground
 
             Console.WriteLine();
             sw.Restart();
-            bc.Encode("72527273070");
+            bc.Encode(barcode);
             Console.WriteLine($"Barcode generated in {sw.ElapsedMilliseconds}ms");
 
             sw.Restart();
@@ -67,6 +77,73 @@ namespace TyKonKet.BarcodeGenerator.Playground
             bc.Dispose();
 
             Console.ReadKey();
+        }
+
+        private static void RenderBarcodes(FontFamilies font)
+        {
+            var type = BarcodeTypes.Code93;
+            var barcode = "012345678905";
+            var height = 30;
+            var scaling = 10;
+            var margins = 2;
+            var backgroundColor = SKColors.White;
+            var foregroundColor = SKColors.Black;
+
+            using var barcode1 = new Barcode(options =>
+            {
+                options.Type = type;
+                options.Height = height;
+                options.Scaling = scaling;
+                options.Margins = margins;
+                options.BackgroundColor = backgroundColor;
+                options.ForegroundColor = foregroundColor;
+                options.RenderText = true;
+                options.UseTypeface(font, SKFontStyle.Normal);
+            });
+            barcode1.Encode(barcode);
+            barcode1.Export(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), $"out/{{barcode}}_{font}_Normal.{{format}}"), quality: 100);
+
+            using var barcode2 = new Barcode(options =>
+            {
+                options.Type = type;
+                options.Height = height;
+                options.Scaling = scaling;
+                options.Margins = margins;
+                options.BackgroundColor = backgroundColor;
+                options.ForegroundColor = foregroundColor;
+                options.RenderText = true;
+                options.UseTypeface(font, SKFontStyle.Bold);
+            });
+            barcode2.Encode(barcode);
+            barcode2.Export(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), $"out/{{barcode}}_{font}_Bold.{{format}}"), quality: 100);
+
+            using var barcode3 = new Barcode(options =>
+            {
+                options.Type = type;
+                options.Height = height;
+                options.Scaling = scaling;
+                options.Margins = margins;
+                options.BackgroundColor = backgroundColor;
+                options.ForegroundColor = foregroundColor;
+                options.RenderText = true;
+                options.UseTypeface(font, SKFontStyle.Italic);
+            });
+            barcode3.Encode(barcode);
+            barcode3.Export(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), $"out/{{barcode}}_{font}_Italic.{{format}}"), quality: 100);
+
+            using var barcode4 = new Barcode(options =>
+            {
+                options.Type = type;
+                options.Height = height;
+                options.Scaling = scaling;
+                options.Margins = margins;
+                options.BackgroundColor = backgroundColor;
+                options.ForegroundColor = foregroundColor;
+                options.RenderText = true;
+                options.UseTypeface(font, SKFontStyle.BoldItalic);
+            });
+            barcode4.Encode(barcode);
+            barcode4.Export(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), $"out/{{barcode}}_{font}_BoldItalic.{{format}}"), quality: 100);
         }
     }
 }
