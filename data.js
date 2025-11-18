7b854194f2f1f586fc96bf73e18cada3d7f61558
github-action-benchmark <github@users.noreply.github.com>
2025-11-18 19:40:19 +0000

    add Ean13Encoder (benchmarkdotnet) benchmark result for f1a4ff07be2d1717155ed47c227be2154b353a37

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763494818409,
+  "lastUpdate": 1763494819581,
   "repoUrl": "https://github.com/TyKonKet/BarcodeGenerator",
   "entries": {
     "Ean8Encoder": [
@@ -8338,6 +8338,54 @@ window.BENCHMARK_DATA = {
             "value": 38634.07596842448,
             "unit": "ns",
             "range": "± 276.09085912705257"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matteoautieri@live.it",
+            "name": "Matteo",
+            "username": "TyKonKet"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f1a4ff07be2d1717155ed47c227be2154b353a37",
+          "message": "Merge pull request #30 from TyKonKet/copilot/add-codabar-barcode-encoder\n\nAdd Codabar barcode encoder",
+          "timestamp": "2025-11-18T20:32:14+01:00",
+          "tree_id": "662764ab1c5ee89fb8848b5f3004f59a48748996",
+          "url": "https://github.com/TyKonKet/BarcodeGenerator/commit/f1a4ff07be2d1717155ed47c227be2154b353a37"
+        },
+        "date": 1763494819153,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "TyKonKet.BarcodeGenerator.CB.Benchmarks.Ean13Encoder.SimpleInstance",
+            "value": 18103.126925150555,
+            "unit": "ns",
+            "range": "± 445.78981339383733"
+          },
+          {
+            "name": "TyKonKet.BarcodeGenerator.CB.Benchmarks.Ean13Encoder.AdvancedInstance",
+            "value": 21990.282621684826,
+            "unit": "ns",
+            "range": "± 483.13887016066764"
+          },
+          {
+            "name": "TyKonKet.BarcodeGenerator.CB.Benchmarks.Ean13Encoder.EncodingWithoutText",
+            "value": 25698.205147879464,
+            "unit": "ns",
+            "range": "± 144.64290422820463"
+          },
+          {
+            "name": "TyKonKet.BarcodeGenerator.CB.Benchmarks.Ean13Encoder.EncodingWithText",
+            "value": 38295.19335065569,
+            "unit": "ns",
+            "range": "± 230.51967530807735"
           }
         ]
       }
