7b8a25a14512218daae6443e4208951863d1ffe6
github-action-benchmark <github@users.noreply.github.com>
2025-11-22 23:18:27 +0000

    add Code128Encoder (benchmarkdotnet) benchmark result for 7b005aca3b2cf91c2cdca89e72bd46f01dd3eaaf

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763853497827,
+  "lastUpdate": 1763853507565,
   "repoUrl": "https://github.com/TyKonKet/BarcodeGenerator",
   "entries": {
     "CodabarEncoder": [
@@ -48,6 +48,56 @@ window.BENCHMARK_DATA = {
             "value": 40324.85782732283,
             "unit": "ns",
             "range": "± 83.19278338774255"
+          }
+        ]
+      }
+    ],
+    "Code128Encoder": [
+      {
+        "commit": {
+          "author": {
+            "email": "matteoautieri@live.it",
+            "name": "TyKonKet",
+            "username": "TyKonKet"
+          },
+          "committer": {
+            "email": "matteoautieri@live.it",
+            "name": "TyKonKet",
+            "username": "TyKonKet"
+          },
+          "distinct": true,
+          "id": "7b005aca3b2cf91c2cdca89e72bd46f01dd3eaaf",
+          "message": "Add GitHub token and configure user for version update commits in benchmark runner",
+          "timestamp": "2025-11-23T00:10:39+01:00",
+          "tree_id": "761044fd07b65b91b1fc7dcf89a7c272354e0d36",
+          "url": "https://github.com/TyKonKet/BarcodeGenerator/commit/7b005aca3b2cf91c2cdca89e72bd46f01dd3eaaf"
+        },
+        "date": 1763853506719,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "TyKonKet.BarcodeGenerator.Benchmarks.Benchmarks.Code128Encoder.SimpleInstance",
+            "value": 458.89899457991123,
+            "unit": "ns",
+            "range": "± 14.290989451684114"
+          },
+          {
+            "name": "TyKonKet.BarcodeGenerator.Benchmarks.Benchmarks.Code128Encoder.AdvancedInstance",
+            "value": 1961.6664380525287,
+            "unit": "ns",
+            "range": "± 42.41923517839884"
+          },
+          {
+            "name": "TyKonKet.BarcodeGenerator.Benchmarks.Benchmarks.Code128Encoder.EncodingWithoutText",
+            "value": 49079.1625,
+            "unit": "ns",
+            "range": "± 343.4923826905731"
+          },
+          {
+            "name": "TyKonKet.BarcodeGenerator.Benchmarks.Benchmarks.Code128Encoder.EncodingWithText",
+            "value": 64546.081843449516,
+            "unit": "ns",
+            "range": "± 81.00212865925492"
           }
         ]
       }
